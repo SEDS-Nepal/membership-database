b0282f3d6db7cf739962eb2ee5d63cb2e8055d3e
Pradipta Shrestha <pradiptashrestha@gmail.com>
2022-11-26 20:32:37 -0600

    Added separate docker-compose for React-nginx

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from .routers import members
 
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="SEDS-Nepal Membership Database")
 app.include_router(members.router)
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8080"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -43,7 +43,7 @@ function Form() {
         const isValid = await userSchema.isValid(formData);
         console.log(isValid);
 
-        await axios.post("http://localhost:8000/api/new_members/",formData)
+        await axios.post("http://localhost:8000/api/members/",formData)
         .then(res => console.log('posting data', res)).catch(err=>console.log(err))
   
  }
