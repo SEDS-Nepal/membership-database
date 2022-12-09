5cec8473bd3cab681c0216dab0f1041396cc9175
Sucharika <106304707+Sucharika@users.noreply.github.com>
2022-12-09 19:03:24 +0545

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings, Field
-
-
-class Settings(BaseSettings):
-    db_url: str = Field(..., env='DATASOURCE_URL')
-
-
-settings = Settings()
--- backend/dbconnection.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from .config import settings
-
-
-engine = create_engine(settings.db_url)
-sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-base = declarative_base()
