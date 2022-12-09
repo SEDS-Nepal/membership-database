b04c306cdf73618c7c84c5f202124ebbed17c0a2
Sucharika <106304707+Sucharika@users.noreply.github.com>
2022-12-09 19:09:15 +0545

    Delete restart.sh

--- restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Stopping docker containers"
-sudo docker-compose -f /home/docker-compose.yml down
-echo "Removing docker container sedsnepal_md_app"
-sudo docker rmi supernovabirth/sedsnepal_md_app
-echo "Removing docker container sedsnepal_md_web"
-sudo docker rmi supernovabirth/sedsnepal_md_web
-echo "Running docker-compose up"
-sudo docker-compose -f /home/docker-compose.yml up >> docker-compose-log.txt
